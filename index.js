e9ef85f0aac9215d533f8420ce7f6ae8dd3b676b
Alberto Schiabel <jkomyno@users.noreply.github.com>
2016-09-29 17:25:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 var h = document.getElementsByClassName('hamburger')[0];
 var o = document.getElementsByClassName('overlay')[0];
 
-
-/* 
-    Explanations:
-    - pi is the length/end point of the cosinus intervall (see above)
-    - newTimestamp indicates the current time when callbacks queued by requestAnimationFrame begin to fire.
-      (for more information see https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame)
-    - newTimestamp - oldTimestamp equals the duration
-
-      a * cos (bx + c) + d                      | c translates along the x axis = 0
-    = a * cos (bx) + d                          | d translates along the y axis = 1 -> only positive y values
-    = a * cos (bx) + 1                          | a stretches along the y axis = cosParameter = window.scrollY / 2
-    = cosParameter + cosParameter * (cos bx)    | b stretches along the x axis = scrollCount = Math.PI / (scrollDuration / (newTimestamp - oldTimestamp))
-    = cosParameter + cosParameter * (cos scrollCount * x)
-*/
-
 h.addEventListener('click', function(e) {
 	e.preventDefault();
 	this.classList.toggle('active');
